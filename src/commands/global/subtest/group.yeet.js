4379c190e346baf61bac3e4a50d2709a7f519f24
George-801 <56756124+George-801@users.noreply.github.com>
2022-05-31 21:30:45 +0100

    remove test files

--- src/commands/global/subtest/group.yeet.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-/** @type {import("../../../types").Command} */
-module.exports = {
-    name: "yeet"
-}
--- src/commands/global/subtestsss.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * @type {import("../../types").Command}
- */
-module.exports = {
-    name: "subtest",
-    description: "test stuff idk"
-}
