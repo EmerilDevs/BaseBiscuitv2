dec10f08aae70808566c68137d60bb11cc5a13c0
George-801 <56756124+George-801@users.noreply.github.com>
2022-05-31 22:44:14 +0100

    command structure caching error fix

--- src/events/registerCommands.js
+++ src/events/registerCommands.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { writeFileSync, readFileSync, existsSync } = require("fs");
+const { writeFileSync, readFileSync, existsSync, readdirSync, unlinkSync } = require("fs");
 const { generateStructure, commands } = require("../handlers/CommandHandler");
 const { info, error} = require("../handlers/LoggingHandler");
 const { getLocalisation: getText } = require("../handlers/LanguageHandler");
@@ -25,11 +25,11 @@ module.exports = {
             // commands have been updated, re-register them
             const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
             try {
-                info(getText(client.consoleLocale, ["events", "registerCommands", "info", "registeringGlobalCommandStructure"]));
+                info(getText(client.consoleLang, ["events", "registerCommands", "info", "registeringGlobalCommandStructure"]));
                 await rest.put(Routes.applicationCommands(client.user.id), { body: generateStructure() });
-                info(getText(client.consoleLocale, ["events", "registerCommands", "info", "registeredGlobalCommandStructure"]));
+                info(getText(client.consoleLang, ["events", "registerCommands", "info", "registeredGlobalCommandStructure"]));
             } catch (e) {
-                error(getText(client.consoleLocale, ["events", "registerCommands", "error", "cannotRegister"], e.stack));
+                error(getText(client.consoleLang, ["events", "registerCommands", "error", "cannotRegister"], e.stack));
             }
         }
         // record current global command structure
@@ -41,15 +41,37 @@ module.exports = {
                 // commands have been updated, re-register them
                 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
                 try {
-                    info(getText(client.consoleLocale, ["events", "registerCommands", "info", "registeringGuildCommandStructure"], guildID));
+                    info(getText(client.consoleLang, ["events", "registerCommands", "info", "registeringGuildCommandStructure"], guildID));
                     await rest.put(Routes.applicationGuildCommands(client.user.id, guildID), { body: generateStructure(guildID) });
-                    info(getText(client.consoleLocale, ["events", "registerCommands", "info", "registeredGuildCommandStructure"], guildID));
+                    info(getText(client.consoleLang, ["events", "registerCommands", "info", "registeredGuildCommandStructure"], guildID));
                 } catch (e) {
-                    error(getText(client.consoleLocale, ["events", "registerCommands", "error", "cannotRegisterGuild"], guildID, e.stack));
+                    error(getText(client.consoleLang, ["events", "registerCommands", "error", "cannotRegisterGuild"], guildID, e.stack));
                 }
             }
             // record current guild command structure
             writeFileSync(join(__dirname, `../cache/structure-${guildID}.json`), JSON.stringify(generateStructure(guildID)), { flag: "w+", encoding: "utf-8" });
+        }
+        // check for guild structures that need to be deleted completely
+        let allGuildIDs = [...new Set(commands.filter(x => x.guild).map(x => x.guild))];
+        // generate regex for all guild ids
+        let guildPattern = new RegExp(`^structure-((?:${allGuildIDs.join(")|(?:")})).json$`, "g");
+        // go through all structure cache files
+        for (const fileName of readdirSync(join(__dirname, "../cache/")).filter(x => !x.includes("global"))) {
+            // if cache file contains guild that is no longer being used
+            if (!fileName.match(guildPattern)?.length) {
+                // unneeded commands
+                let guildID = /structure-([0-9]+)/g.exec(fileName)[1];
+                const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+                try {
+                    info(getText(client.consoleLang, ["events", "registerCommands", "info", "registeringGuildCommandStructure"], guildID));
+                    await rest.put(Routes.applicationGuildCommands(client.user.id, guildID), { body: generateStructure(guildID) });
+                    // delete cache file
+                    unlinkSync(join(__dirname, `../cache/structure-${guildID}.json`));
+                    info(getText(client.consoleLang, ["events", "registerCommands", "info", "registeredGuildCommandStructure"], guildID));
+                } catch (e) {
+                    error(getText(client.consoleLang, ["events", "registerCommands", "error", "cannotRegisterGuild"], guildID, e.stack));
+                }
+            }
         }
     }
 }
